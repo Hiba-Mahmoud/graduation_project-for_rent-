2a4727a902bc909ea7b9b2d914c749306d372096
hiba <hibamahmudali@gmail.com>
2022-07-30 01:55:44 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
@@ -8,6 +7,10 @@ import { PropertyComponent } from './userActions/component/property/property.com
 import { FavouriteComponent } from './userActions/component/favourite/favourite.component';
 import { CreditCardComponent } from './userActions/component/credit-card/credit-card.component';
 import { AddCreditCardComponent } from './userActions/component/add-credit-card/add-credit-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
@@ -16,11 +19,17 @@ import { AddCreditCardComponent } from './userActions/component/add-credit-card/
     PropertyComponent,
     FavouriteComponent,
     CreditCardComponent,
-    AddCreditCardComponent
+    AddCreditCardComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    NgbModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
