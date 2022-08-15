aaf6a2521e03424743008b5d3b2cc801e59bb82a
Eman Ahmed <eman455sallam@gmail.com>
2022-08-15 18:23:43 +0200

    eman

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
+import { Observable } from 'rxjs';
 import { AdvertismentService } from '../shared/services/advertisment.service';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,8 +14,15 @@ export class HomeComponent implements OnInit {
    totalNumber:any;
    page:number=1;
    loading:boolean=true;
+
+   allLocations:any;
+   allTypes:any;
+   bedsNumber:any;
+   location:any;
+   type:any;
+   beds:any;
   
-  constructor(private advertismentService:AdvertismentService) { }
+  constructor(private advertismentService:AdvertismentService ,private http: HttpClient) { }
 
   ngOnInit(): void {
     this.advertismentService.getAll("http://127.0.0.1:8000/api/Home").subscribe((res)=>{
@@ -22,11 +33,61 @@ export class HomeComponent implements OnInit {
         console.log(this.allAdvert[2].advertisement_image[0].image_name);
         console.log(this.totalNumber);
      })
+
+     this.getAllLocations();
+     this.getAllTypes();
+     this.getBedsNumber();
+
+     
   }
+  getAllLocations(){
+    this.advertismentService.getAllLocations().subscribe((res)=>{
+     this.allLocations=res.city_choices;
+  })
+}
+
+  
+getAllTypes(){
+  this.advertismentService.getAllTypes().subscribe((res)=>{
+   this.allTypes=res.type_choices;
+})
+}
+
+
+getBedsNumber(){
+  this.advertismentService.getBedsNumber().subscribe((res)=>{
+    this.bedsNumber=res.bedroom_number_choices;
+
+}) 
 
 
 }
 
 
+getFilterLocation(event:MatSelectChange){
+  this.location=event.value;
+}
+
+getFilterType(event:MatSelectChange){
+  this.type=event.value;
+
+}
+getFilterBeds(event:MatSelectChange){
+    this.beds=event.value;
+
+}
+
+ getBySearch() { 
+
+   this.advertismentService.getBySearch({"city_id": this.location || "", "bedroom_num": this.beds || " " ,"type":this.type || " "} ).subscribe((res)=>{
+  console.log(res);
+}) 
+ }
 
 
+
+
+
+
+
+}
