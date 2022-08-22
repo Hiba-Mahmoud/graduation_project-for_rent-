fbe8f9ff1879c4a45da05ed56d45cc21ef294ff9
Eman Ahmed <eman455sallam@gmail.com>
2022-08-23 01:37:49 +0200

    eman

--- src/app/users/component/renter/renter.component.ts
+++ src/app/users/component/renter/renter.component.ts
@@ -43,7 +43,6 @@ export class RenterComponent implements OnInit {
 
       })
   }
-
   getAllLocations() {
     this.advertismentService.getAllLocations().subscribe((res) => {
          this.allLocations = res.cities;
